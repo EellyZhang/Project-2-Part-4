123f1db0a0b31e0456054b3710584d14ab0dfdff
EellyZhang <31869183+EellyZhang@users.noreply.github.com>
2017-11-16 19:05:39 -0500

    Add files via upload

---
+++ CubeBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+
+public class CubeBehavior : MonoBehaviour {
+	public int x, y;
+	public bool isPlane;
+
+	// Use this for initialization
+	void Start () {
+		GameController.activatePlane = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+		
+	void OnMouseDown() {
+		//print("isPlane: "+isPlane);
+
+		if (GameController.activatePlane) {
+			if (isPlane) {
+				GameController.PlaneDeactivation ();
+			}
+		}
+		else if (isPlane) {
+			GameController.PlaneActivation ();
+			print("x: " + x + "....y: " + y);
+		}
+	}
+}
